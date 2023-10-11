f77a8a02c35ec2bee59120a078f2821acab3e9ca
jeffsui <pinghailinfeng79@gmail.com>
2023-10-11 17:09:37 +0800

    login and register

--- src/main.js
+++ src/main.js
@@ -21,12 +21,80 @@ registerBtn.addEventListener("click", function (e) {
 // 提交登录表单 跳转到chatPage
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  showPage("chatPage");
+  const loginEmail = document.querySelector("#login_email");
+  const loginPass = document.querySelector("#login_password");
+  console.log({ email: loginEmail.value,
+    password: loginPass.value});
+  //后台发送Post请求,邮箱/密码 验证是否正确 fetch /auth/login
+  /**
+   * 定义请求登录API进行验证(异步请求)
+   * @param {请求路径} path
+   * @param {发送json格式的数} body
+   */
+  const apiCall = (path, body) => {
+    fetch("http://localhost:5005/" + path, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          //请求失败,服务器给你返回的数据
+          alert(data.error);
+        } else {
+          //验证通过
+          console.log("success:", data);
+          showPage("chatPage");
+        }
+      });
+  };
+  //调用封装登录请求
+  apiCall("auth/login", {
+    email: loginEmail.value,
+    password: loginPass.value,
+  });
 });
 
 registerForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  showPage("loginPage");
+  //获取表单元素
+  const regEmail  = document.getElementById("regemail");
+  const regName  = document.getElementById("regname");
+  const regPass = document.getElementById("regpassword");
+  const confirmPass = document.getElementById("confirmpassword");
+
+  if(regPass.value !== confirmPass.value){
+    alert("Confirm password not match!");
+  }
+  const apiCall = (path, body) => {
+    fetch("http://localhost:5005/" + path, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          //请求失败,服务器给你返回的数据
+          alert(data.error);
+        } else {
+          //注册通过
+          console.log("success:", data);
+          showPage("loginPage");
+        }
+      });
+  };
+  apiCall("auth/register",{
+    "email": regEmail.value,
+    "password": regPass.value,
+    "name": regName.value
+  })
+
 });
 
 //实现spa 页面逻辑
